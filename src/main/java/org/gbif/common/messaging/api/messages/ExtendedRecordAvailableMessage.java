19c8c0cd257fd737f5c12d44d58023a804b0c424
Nikolay Volik <nvolik@gbif.org>
2018-04-16 16:50:51 +0200

    #94 pipelines Added new value

--- src/main/java/org/gbif/common/messaging/api/messages/ExtendedRecordAvailableMessage.java
+++ src/main/java/org/gbif/common/messaging/api/messages/ExtendedRecordAvailableMessage.java
@@ -1,11 +1,12 @@
 package org.gbif.common.messaging.api.messages;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
@@ -15,16 +16,18 @@ public class ExtendedRecordAvailableMessage implements DatasetBasedMessage{
   public static final String ROUTING_KEY = "crawl.conversion.finished";
 
   private final UUID datasetUuid;
-  private final URI hdfsPath;
-
+  private final URI inputFile;
+  private final String[] interpretTypes;
 
   @JsonCreator
   public ExtendedRecordAvailableMessage(
     @JsonProperty("datasetUuid") UUID datasetUuid,
-    @JsonProperty("hdfsPath") URI hdfsPath
+    @JsonProperty("inputFile") URI inputFile,
+    @JsonProperty("interpretTypes") String[] interpretTypes
   ) {
     this.datasetUuid = checkNotNull(datasetUuid, "datasetUuid can't be null");
-    this.hdfsPath = checkNotNull(hdfsPath, "hdfsPath can't be null");
+    this.inputFile = checkNotNull(inputFile, "inputFile can't be null");
+    this.interpretTypes = checkNotNull(interpretTypes, "interpretTypes can't be null");
   }
 
   /**
@@ -39,25 +42,32 @@ public class ExtendedRecordAvailableMessage implements DatasetBasedMessage{
    *
    * @return path of final dataset in avro format
    */
-  public URI getHdfsPath() {
-    return hdfsPath;
+  public URI getInputFile() {
+    return inputFile;
+  }
+
+  /**
+   * @return types of interpretation - ALL, LOCATION, BASE or etc.
+   */
+  public String[] getInterpretTypes() {
+    return interpretTypes;
   }
 
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
-
     ExtendedRecordAvailableMessage that = (ExtendedRecordAvailableMessage) o;
-
-    if (!datasetUuid.equals(that.datasetUuid)) return false;
-    return hdfsPath.equals(that.hdfsPath);
+    return Objects.equals(datasetUuid, that.datasetUuid) && Objects.equals(inputFile, that.inputFile) && Arrays.equals(
+      interpretTypes,
+      that.interpretTypes);
   }
 
   @Override
   public int hashCode() {
-    int result = datasetUuid.hashCode();
-    result = 31 * result + hdfsPath.hashCode();
+
+    int result = Objects.hash(datasetUuid, inputFile);
+    result = 31 * result + Arrays.hashCode(interpretTypes);
     return result;
   }
 
