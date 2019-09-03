3196cb49631560475b9bf054b15eb01090baf039
fmendezh <federicomh@gmail.com>
2019-09-03 12:05:52 +0200

    adding parseOrDefault method

--- src/main/java/org/gbif/common/messaging/api/messages/Platform.java
+++ src/main/java/org/gbif/common/messaging/api/messages/Platform.java
@@ -18,7 +18,22 @@ public enum Platform {
     return this == platform || Platform.ALL == this;
   }
 
+  /**
+   * Parse a string value into a recognized {@link Platform} instance.
+   * @param platform value to be parsed
+   * @return an Optional instance with the parsed value if exists
+   */
   public static Optional<Platform> parse(String platform) {
     return Optional.ofNullable(platform).map(s ->  Platform.valueOf(s.trim().toUpperCase()));
+  }
+
+  /**
+   * Parse a string value into a {@link Platform} instance, if not instance is recognized a default value is returned.
+   * @param platform value to be parsed
+   * @param defaultValue default
+   * @return parsed platform value or defaultValue
+   */
+  public static Platform parseOrDefault(String platform, Platform defaultValue) {
+    return Optional.ofNullable(platform).map(s ->  Platform.valueOf(s.trim().toUpperCase())).orElse(defaultValue);
   }
 }
