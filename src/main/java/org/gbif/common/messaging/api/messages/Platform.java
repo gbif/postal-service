4a16ead48788c45121e8576d1443dfe9daa99fcc
fmendezh <federicomh@gmail.com>
2019-09-03 09:19:35 +0200

    adding javadoc

--- src/main/java/org/gbif/common/messaging/api/messages/Platform.java
+++ src/main/java/org/gbif/common/messaging/api/messages/Platform.java
@@ -1,11 +1,18 @@
 package org.gbif.common.messaging.api.messages;
 
+/**
+ * Enumerate the indexing platforms.
+ * This is later used to decide what platform processes a message.
+ */
 public enum Platform {
   PIPELINES,
   OCCURRENCE,
   ALL;
 
+  /**
+   * Valdiates if a platforms is equivalent to another, i.e: are the same enum or else is {@link Platform#ALL}
+   */
   public boolean equivalent(Platform platform) {
-    return Platform.ALL == this || this == platform;
+    return this == platform || Platform.ALL == this;
   }
 }
