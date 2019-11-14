e6dffff82955f062df96e0cbf29b84e07b9bd689
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-11-14 12:18:25 +0100

    added message to send when HDFS view finishes

---
+++ src/main/java/org/gbif/common/messaging/api/messages/PipelinesHdfsViewBuiltMessage.java
@@ -0,0 +1,125 @@
+package org.gbif.common.messaging.api.messages;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * This message indicates that the HDFS view of a dataset has finished.
+ */
+public class PipelinesHdfsViewBuiltMessage implements PipelineBasedMessage {
+
+  public static final String ROUTING_KEY = "occurrence.pipelines.hdfsview.finished";
+
+  private UUID datasetUuid;
+  private int attempt;
+  private Set<String> pipelineSteps;
+  private String runner;
+
+  public PipelinesHdfsViewBuiltMessage() {
+  }
+
+  @JsonCreator
+  public PipelinesHdfsViewBuiltMessage(
+      @JsonProperty("datasetUuid") UUID datasetUuid,
+      @JsonProperty("attempt") int attempt,
+      @JsonProperty("pipelineSteps") Set<String> pipelineSteps,
+      @JsonProperty("runner") String runner) {
+    this.datasetUuid = checkNotNull(datasetUuid, "datasetUuid can't be null");
+    checkArgument(attempt >= 0, "attempt has to be greater than 0");
+    this.attempt = attempt;
+    this.pipelineSteps = pipelineSteps == null ? Collections.emptySet() : pipelineSteps;
+    this.runner = runner;
+  }
+
+  public PipelinesHdfsViewBuiltMessage(
+      UUID datasetUuid,
+      int attempt,
+      Set<String> pipelineSteps) {
+    this(datasetUuid, attempt, pipelineSteps, null);
+  }
+
+  @Override
+  public UUID getDatasetUuid() {
+    return datasetUuid;
+  }
+
+  @Override
+  public Integer getAttempt() {
+    return attempt;
+  }
+
+  @Override
+  public Set<String> getPipelineSteps() {
+    return pipelineSteps;
+  }
+
+  @Override
+  public String getRoutingKey() {
+    return ROUTING_KEY;
+  }
+
+  public String getRunner() {
+    return runner;
+  }
+
+  public PipelinesHdfsViewBuiltMessage setDatasetUuid(UUID datasetUuid) {
+    this.datasetUuid = datasetUuid;
+    return this;
+  }
+
+  public PipelinesHdfsViewBuiltMessage setAttempt(int attempt) {
+    this.attempt = attempt;
+    return this;
+  }
+
+  public PipelinesHdfsViewBuiltMessage setPipelineSteps(Set<String> pipelineSteps) {
+    this.pipelineSteps = pipelineSteps;
+    return this;
+  }
+
+  public PipelinesHdfsViewBuiltMessage setRunner(String runner) {
+    this.runner = runner;
+    return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PipelinesHdfsViewBuiltMessage that = (PipelinesHdfsViewBuiltMessage) o;
+    return attempt == that.attempt &&
+        Objects.equals(datasetUuid, that.datasetUuid) &&
+        Objects.equals(pipelineSteps, that.pipelineSteps) &&
+        Objects.equals(runner, that.runner);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(datasetUuid, attempt, pipelineSteps, runner);
+  }
+
+  @Override
+  public String toString() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      return objectMapper.writeValueAsString(this);
+    } catch (IOException e) {
+      // NOP
+    }
+    return "";
+  }
+}
