acb26aecd3695ef435594283bef3376bc2b5ef20
fmendezh <federicomh@gmail.com>
2022-10-27 14:58:38 +0200

    removing unused message https://github.com/gbif/pipelines/issues/803

--- src/main/java/org/gbif/common/messaging/DefaultMessageRegistry.java
+++ src/main/java/org/gbif/common/messaging/DefaultMessageRegistry.java
@@ -102,7 +102,6 @@ public class DefaultMessageRegistry implements MessageRegistry {
     messageToExchangeMappingInternal.put(PipelinesEventsHdfsViewBuiltMessage.class, "occurrence");
     messageToExchangeMappingInternal.put(VocabularyReleasedMessage.class, "vocabulary");
     messageToExchangeMappingInternal.put(CamtrapDpDownloadFinishedMessage.class, "crawler");
-    messageToExchangeMappingInternal.put(PipelinesCamtrapDpMessage.class, "crawler");
     MESSAGE_TO_EXCHANGE_MAPPING = Collections.unmodifiableMap(messageToExchangeMappingInternal);
 
     Map<Class<? extends Message>, String> messageToRoutingKeyMapping = new HashMap<>();
@@ -182,8 +181,6 @@ public class DefaultMessageRegistry implements MessageRegistry {
         PipelinesEventsHdfsViewBuiltMessage.class, PipelinesEventsHdfsViewBuiltMessage.ROUTING_KEY);
     messageToRoutingKeyMapping.put(
       CamtrapDpDownloadFinishedMessage.class, CamtrapDpDownloadFinishedMessage.ROUTING_KEY);
-    messageToRoutingKeyMapping.put(
-      PipelinesCamtrapDpMessage.class, CamtrapDpDownloadFinishedMessage.ROUTING_KEY);
     MESSAGE_TO_ROUTING_KEY_MAPPING = Collections.unmodifiableMap(messageToRoutingKeyMapping);
   }
 
